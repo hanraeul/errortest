0f289b0596677849014ec54b6095fb81cea00b09
hanraeul <toast17@naver.com>
2024-05-17 16:42:55 +0900

    init

---
+++ src/main/java/com/sparta/errortest/ErrortestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.errortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrortestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrortestApplication.class, args);
+    }
+
+}
